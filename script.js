58f5df3e35d4a269af9c77631ca7368cef9c3638
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-08-19 17:10:24 +0200

    add setMenuOpen function to manage menu button state and submenu visibility

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 
 const menuButton = document.querySelector("#menu-button");
 const submenu = document.querySelector("#main-submenu");
+
+function setMenuOpen(isOpen) {
+    menuButton.setAttribute("aria-expanded", String(isOpen));
+    submenu.hidden = !isOpen;
+}

8b7f08cc7f138f6abed7c2f5d1754a5640bcf237
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-08-19 17:18:39 +0200

    add event listener to close menu when clicking outside of it

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ menuButton.addEventListener("click", () => {
     const isOpen = menuButton.getAttribute("aria-expanded") === "true";
     setMenuOpen(!isOpen);
 });
+
+document.addEventListener("click", (event) => {
+    const clickedInsideMenu = event.target.closest(".menu-wrapper");
+
+    if (!clickedInsideMenu) {
+        setMenuOpen(false);
+    }
+});
+

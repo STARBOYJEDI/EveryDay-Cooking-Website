4deba0ac6c23917369594ca7b08d97315ab240e0
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-08-24 21:53:09 +0200

    add search button and popup variable for search functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
 const menuButton = document.querySelector("#menu-button");
 const submenu = document.querySelector("#main-submenu");
+const searchButton = document.querySelector("#search-button");
+const searchPopup =
 
 function setMenuOpen(isOpen) {
     menuButton.setAttribute("aria-expanded", String(isOpen));

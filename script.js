65038494bfc6bce10f7f3064f61cfdc5045e2ccd
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-08-27 18:10:19 +0200

    fix: add missing search input selector in script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const menuButton = document.querySelector("#menu-button");
 const submenu = document.querySelector("#main-submenu");
 const searchButton = document.querySelector("#search-button");
 const searchPopup = document.querySelector("#search-popup");
+const searchInput = document.querySelector("#site-search");
 const searchCloseButton = document.querySelector("#search-close-button");
 
 function setMenuOpen(isOpen) {
